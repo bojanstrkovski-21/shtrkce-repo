dfd9e811e4f8d22b0061c30b09c270d002db9e50
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2024-05-21 21:08:25 +0200

    update

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+#rm shtrkce-repo*
+
+#echo "repo-add"
+#repo-add -n -R shtrkce-repo.db.tar.gz *.pkg.tar.zst
+
+#!/bin/bash
+
 rm shtrkce-repo*
 
 echo "repo-add"
 repo-add -n -R shtrkce-repo.db.tar.gz *.pkg.tar.zst
+
+sleep 1
+
+rm shtrkce-repo.db
+
+rm shtrkce-repo.files
+
+mv shtrkce-repo.db.tar.gz shtrkce-repo.db
+
+mv shtrkce-repo.files.tar.gz shtrkce-repo.files
 
 
 echo "####################################"
